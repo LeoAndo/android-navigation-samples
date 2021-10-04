616ed5474e54c2fc1831893eaf028bb6d85c87fb
cmStudent <cmstudent@cmAir059.local>
2021-10-04 18:45:40 +0900

    手順４ FragmentからDialog表示(Navigation使用)

--- NavigationJavaSample/app/src/main/java/com/example/navigationjavasample/ui/dashboard/DashboardFragment.java
+++ NavigationJavaSample/app/src/main/java/com/example/navigationjavasample/ui/dashboard/DashboardFragment.java
@@ -1,35 +1,37 @@
 package com.example.navigationjavasample.ui.dashboard;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.navigationjavasample.R;
+import com.example.navigationjavasample.databinding.FragmentDashboardBinding;
+
+import static androidx.navigation.fragment.NavHostFragment.findNavController;
 
 public class DashboardFragment extends Fragment {
+    private FragmentDashboardBinding binding;
 
-    private DashboardViewModel dashboardViewModel;
+    public DashboardFragment() {
+        super(R.layout.fragment_dashboard);
+    }
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        binding = FragmentDashboardBinding.bind(view);
+        binding.button.setOnClickListener((v) -> {
+            findNavController(DashboardFragment.this).navigate(
+                    DashboardFragmentDirections.actionNavigationDashboardToFooDialogFragment()
+            );
         });
-        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
